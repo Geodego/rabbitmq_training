4edb40e90e41412f9d7a704e0e648255fab54ded
G de Gournay <geoffroydegournay@hotmail.com>
2022-06-21 15:54:24 +0200

    feat: sending done

--- hello_world/send.py
+++ hello_world/send.py
@@ -3,8 +3,18 @@ This program sends a single message to the queue
 
 June 21, 2022
 """
+import pika
 
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+channel = connection.channel()
 
+# We're connected now, to a broker on the local machine - hence the localhost.
+# If we wanted to connect to a broker on a different machine we'd simply specify its name or IP address here.
 
+channel.queue_declare(queue="hello")
 
-    
+channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+print(" [x] Sent 'Hello World!'")
+
+connection.close()
+
