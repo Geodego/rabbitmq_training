6d5370ac1195764d9d0cbe04e9fbe0d738fa1b39
G de Gournay <geoffroydegournay@hotmail.com>
2022-06-21 16:15:49 +0200

    style: minor modification

--- hello_world/send.py
+++ hello_world/send.py
@@ -14,7 +14,7 @@ channel = connection.channel()
 channel.queue_declare(queue="hello")
 
 channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-print(" [x] Sent 'Hello World!'")
+print(" A Sent 'Hello World!'")
 
 connection.close()
 
